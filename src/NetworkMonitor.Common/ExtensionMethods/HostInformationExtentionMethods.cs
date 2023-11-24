80a90b8f8abb59b03f358b3b0b9638933a5d1a8f
DruiD <omsk.petr@gmail.com>
2023-11-24 22:50:44 +0600

    Update

--- src/NetworkMonitor.Common/ExtensionMethods/HostInformationExtentionMethods.cs
+++ src/NetworkMonitor.Common/ExtensionMethods/HostInformationExtentionMethods.cs
@@ -7,27 +7,27 @@ public static class HostInformationExtensionMethods
 {
     public static string Log(this HostInformation hostInformation)
     {
-        var result = $"Gateway - {hostInformation.Gateway} {Environment.NewLine}";
-        result += $"HostName - {hostInformation.HostName} {Environment.NewLine}";
-        result += $"IPv4Address - {hostInformation.IPv4Address} {Environment.NewLine}";
-        result += $"DHCP - {hostInformation.Dhcp} {Environment.NewLine}";
-        result += $"DNS сервера: {Environment.NewLine}";
+        var result = $"Gateway - {hostInformation.Gateway}{Environment.NewLine}";
+        result += $"HostName - {hostInformation.HostName}{Environment.NewLine}";
+        result += $"IPv4Address - {hostInformation.IPv4Address}{Environment.NewLine}";
+        result += $"DHCP - {hostInformation.Dhcp}{Environment.NewLine}";
+        result += $"DNS сервера:{Environment.NewLine}";
 
         foreach (var address in hostInformation.DnsList)
         {
-            result += $"{address} {Environment.NewLine}";
+            result += $"{address}{Environment.NewLine}";
         }
 
         result += $"Таблица трассировки: {Environment.NewLine}";
         foreach (var address in hostInformation.TracertTable)
         {
-             result += $"{address} {Environment.NewLine}";
+             result += $"{address}{Environment.NewLine}";
         }
 
         result += $"Таблица ARP: {Environment.NewLine}";
         foreach (var address in hostInformation.ArpTable)
         {
-            result += $"{address.IpAddress} - {address.MacAddress} {Environment.NewLine}";
+            result += $"{address.IpAddress} - {address.MacAddress}{Environment.NewLine}";
         }
 
         return result;
